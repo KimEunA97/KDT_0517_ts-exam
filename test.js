b4e488d0334234be74a03a8ecea5b9953b2ffb26
KimEunA97 <kimeuna9712@gmail.com>
2023-05-17 11:01:45 +0900

    전개연산자

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ function first(arrayData) {
     //1. 첫번째 배열 인덱스의 값을 변환하는 함수를 만들고 싶다.
     var result = "강지민";
     var arr = [];
-    //unshift는 배열 앞에 놓겠다. 
-    arrayData.unshift(result);
+    //전개연산자, reduce, map, push, 0번째 바꾸기
+    arr.push(result);
     return arr;
 }
 var data = ["최대건", "정성철", "허진", "이세민"];
--- test.ts
+++ test.ts
@@ -3,14 +3,16 @@
 // * typeof(arrayData) === string
 function first(arrayData: string[]): string[] {
   //1. 첫번째 배열 인덱스의 값을 변환하는 함수를 만들고 싶다.
-  let result: string = "강지민";
+  let result: string[] = ["강지민","김동주","김은아"];
   let arr: string[] = [];
+  //전개연산자, reduce, map, push, 0번째 바꾸기
+  arr.push(...result)
 
-  //unshift는 배열 앞에 놓겠다. 
-  arr.unshift(result);
+
   return arr;
 }
 
 let data = ["최대건", "정성철", "허진", "이세민"]
 
 console.log(first(data))
+console.log(first.length);
